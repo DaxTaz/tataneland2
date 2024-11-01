da7f3c9766e523a61a6687304484c54a8bfea7a0
daxtaz <daxtaz@gmail.com>
2024-11-01 20:58:38 +0100

    remove name field

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-INSERT INTO USERS (user_id, name, email, password) VALUES (1, 'userName1', 'userEmail1@email.com', 'user1Password');
-INSERT INTO USERS (user_id, name, email, password) VALUES (2, 'userName2', 'userEmail2@email.com', 'user2Password');
-
---retirer name ca sert à rien
+INSERT INTO USERS (user_id, email, password) VALUES (1, 'userEmail1@email.com', 'user1Password');
+INSERT INTO USERS (user_id, email, password) VALUES (2, 'userEmail2@email.com', 'user2Password');
