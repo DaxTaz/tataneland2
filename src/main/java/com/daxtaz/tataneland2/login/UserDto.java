4f3751f90c96934e91947ab0549bdbcba5c54252
daxtaz <daxtaz@gmail.com>
2024-11-01 20:59:48 +0100

    remove name field

--- src/main/java/com/daxtaz/tataneland2/login/UserDto.java
+++ src/main/java/com/daxtaz/tataneland2/login/UserDto.java
@@ -4,8 +4,6 @@ public class UserDto {
 	
 	private Integer id;
 
-	private String name;
-	
 	private String email;
 	
 	private String password;
@@ -16,14 +14,6 @@ public class UserDto {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getEmail() {
