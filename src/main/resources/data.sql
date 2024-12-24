5e2339bc234845548a08399c465bf2acf68a6d5c
daxtaz <daxtaz@gmail.com>
2024-12-24 10:08:59 +0100

    Rempove actor data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO USERS (user_id, email, password) VALUES (1, 'userEmail1@email.com', 'user1Password');
 INSERT INTO USERS (user_id, email, password) VALUES (2, 'userEmail2@email.com', 'user2Password');
+
+
+--INSERT INTO ACTORS (actor_id, name, nationality, AGE_OF_BIRTH, biography) VALUES (1, 'Actor1', 'French', '2024-12-01', 'Biography actor 1');
+--INSERT INTO ACTORS (actor_id, name, nationality, AGE_OF_BIRTH, biography) VALUES (2, 'Actor2', 'English', '2024-12-02', 'Biography actor 2');
