7de7f0154260a6156748d8fb1bc828123900e724
daxtaz <daxtaz@gmail.com>
2025-01-30 10:32:12 +0100

    now we can remove or not image when mivie was updated

--- src/main/java/com/daxtaz/tataneland2/movie/IMovieDao.java
+++ src/main/java/com/daxtaz/tataneland2/movie/IMovieDao.java
@@ -1,11 +1,25 @@
 package com.daxtaz.tataneland2.movie;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMovieDao extends JpaRepository<Movie, Integer> {
-
+	
 	public Movie findByName(String movieName);
+	
+	public void findByWriter(String writer);
+	
+	@Modifying
+	@Query(value = "update movies set name = :name where movie_id = :id", nativeQuery = true)
+	public void updateMovieWithoutImage(@Param(value = "name") String name, @Param(value = "id") Integer id);
+	
+	//@Query(value = "update movies set name = :name where movie_id = :id ", nativeQuery=true)
+	//public void test(@Param(value = "name") String name, @Param(value = "id") Integer id);
 	
 }
--- src/main/java/com/daxtaz/tataneland2/movie/IMovieService.java
+++ src/main/java/com/daxtaz/tataneland2/movie/IMovieService.java
@@ -1,18 +1,23 @@
 package com.daxtaz.tataneland2.movie;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
 
 public interface IMovieService {
 	
-	public Movie saveMovie(Movie movie);
+	public Movie saveMovie(Movie movie, MultipartFile imageData);
 	
-	public Movie updateMovie(Movie movie);
+	public Movie updateMovie(Boolean deleteImage, Movie movie, MultipartFile imageData);
 	
 	public void deleteMovie(Integer id);
 	
 	public List<Movie> findAll();
 	
 	public Movie findByName(String movieName);
+	
+	public Optional<Movie> findById(Integer id);
 	
 	public boolean checkIfIdExist(Integer movieId);
 
