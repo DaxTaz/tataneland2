4348aa856b6f03a7e9c8df0f14d84535945ff7ee
daxtaz <daxtaz@gmail.com>
2025-01-30 10:34:02 +0100

    now we can remove image when movie was updated

--- src/main/java/com/daxtaz/tataneland2/movie/MovieServiceImpl.java
+++ src/main/java/com/daxtaz/tataneland2/movie/MovieServiceImpl.java
@@ -1,11 +1,14 @@
 package com.daxtaz.tataneland2.movie;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service("MovieService")
 @Transactional
@@ -19,11 +22,34 @@ public class MovieServiceImpl implements IMovieService {
 		this.movieDao = movieDao;
 	}
 
-	public Movie saveMovie(Movie movie) {
+	public Movie saveMovie(Movie movie, MultipartFile imageData) {
+		
+		try {
+			movie.setImageData(imageData.getBytes());
+		} catch (IOException e) {
+			System.out.println("#####################Error movie set imagae data");
+		}
+		
 		return movieDao.save(movie);
 	}
 	
-	public Movie updateMovie(Movie movie) {
+	public Movie updateMovie(Boolean deleteImage, Movie movie, MultipartFile imageData) {
+		
+		Optional<Movie> movieToUpdate = movieDao.findById(movie.getId());
+		
+		if(imageData.getSize() != 0) {
+			try {
+				movie.setImageData(imageData.getBytes());
+			} catch (IOException e) {
+				System.out.println("#####################Error movie set imagae data");
+			}
+		} else {
+			if(deleteImage)
+				movie.setImageData(null);
+			else
+				movie.setImageData(movieToUpdate.get().getImageData());
+		}
+		
 		return movieDao.save(movie);
 	}
 	
@@ -37,6 +63,10 @@ public class MovieServiceImpl implements IMovieService {
 	
 	public Movie findByName(String movieName) {
 		return movieDao.findByName(movieName);
+	}
+	
+	public Optional<Movie> findById(Integer id) {
+		return movieDao.findById(id);
 	}
 	
 	public boolean checkIfIdExist(Integer movieId) {
